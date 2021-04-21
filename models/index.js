bbcbd255d1cac2ba72de280a4a8a9334f6990642
jessicap5159 <jessicaj5159@gmail.com>
2021-04-21 12:20:44 -0700

    created api routes for post model

--- models/index.js
+++ models/index.js
@@ -3,4 +3,13 @@
 const User = require('./User'); // Import the User model
 const Post = require('./Post');
 
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post }; // Export an object with the User model as a property
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
 
+const userRoutes = require('./api/user-routes');
+const postRoutes = require('./api/post-routes');
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
