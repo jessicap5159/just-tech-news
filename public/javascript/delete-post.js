64f004a6e819232fcd5405684e8b95f9438b46f1
jessicap5159 <jessicaj5159@gmail.com>
2021-04-30 11:46:09 -0700

    create an edit post page

---
+++ public/javascript/delete-post.js
@@ -0,0 +1,5 @@
+async function deleteFormHandler(event) {
+    event.preventDefault();
+}
+
+document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
---
+++ utils/auth.js
@@ -0,0 +1,12 @@
+const { response } = require("express")
+
+const withAuth = (req,res,next) => {
+    if (!req.session.user_id) {
+        res.redirect('/login');
+
+    } else {
+        next();
+    }
+};
+
+module.exports = withAuth;
